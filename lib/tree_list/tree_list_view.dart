7210e8804760b8d80bb3607c141f50f9f5f4e46f
Sergio Moretti <sergio@reliqs.net>
2021-04-05 13:44:23 +0200

    tree list refactoring

--- lib/tree_list/tree_list_view.dart
+++ lib/tree_list/tree_list_view.dart
@@ -1,15 +1,32 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'model.dart';
 
-abstract class TreeListTile<E extends TreeNode> {
+abstract class TreeListTile<E extends TreeNode> extends StatelessWidget {
   final E node;
   final int rootLevel;
+  final double levelPadding;
+  final IconData nodeExpandedIcon;
+  final IconData nodeCollapsedIcon;
+  final Widget? title;
+  final Widget? subtitle;
+  final Widget? leading;
   final void Function(BuildContext context, E node)? onRemove;
   final void Function(BuildContext context, E parent, E node)? onAdd;
 
-  TreeListTile({this.onRemove, this.onAdd, required this.node, required this.rootLevel});
+  TreeListTile({this.title, this.subtitle, this.leading,
+      this.levelPadding = 10.0,
+      this.nodeExpandedIcon = Icons.arrow_drop_down,
+      this.nodeCollapsedIcon = Icons.arrow_right,
+      this.onRemove,
+      this.onAdd,
+      required this.node,
+      required this.rootLevel}) : super(
+  );
 
   Widget buildTitle(BuildContext context, TreeListModel<E> model);
 
@@ -19,21 +36,21 @@ abstract class TreeListTile<E extends TreeNode> {
     if (model.editable) {
       return Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
         IconButton(
-          icon: Icon(node.expanded ? Icons.arrow_drop_down : Icons.arrow_right),
+          icon: Icon(node.expanded ? nodeExpandedIcon : nodeCollapsedIcon),
           onPressed: () => Provider.of<TreeListModel<E>>(context, listen: false).toggleExpandNode(node),
         ),
-        Container(padding: EdgeInsets.symmetric(horizontal: (node.level - rootLevel) * 10.0)),
+        Container(padding: EdgeInsets.symmetric(horizontal: (node.level - rootLevel) * levelPadding)),
       ]);
     } else {
       return Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
         IconButton(
-          icon: Icon(node.expanded ? Icons.arrow_drop_down : Icons.arrow_right),
+          icon: Icon(node.expanded ? nodeExpandedIcon : nodeCollapsedIcon),
           onPressed: () => Provider.of<TreeListModel<E>>(context, listen: false).toggleExpandNode(node),
         ),
         IconButton(icon: const Icon(Icons.add), onPressed: () => _onAdd(context, node)),
         IconButton(icon: const Icon(Icons.delete), onPressed: () => _onRemove(context, node)),
-        Container(padding: EdgeInsets.symmetric(horizontal: (node.level - rootLevel) * 10.0)),
-        Icon(Icons.arrow_right),
+        Container(padding: EdgeInsets.symmetric(horizontal: (node.level - rootLevel) * levelPadding)),
+        Icon(nodeCollapsedIcon),
       ]);
     }
     // Icon(Icons.arrow_right),
@@ -50,7 +67,6 @@ abstract class TreeListTile<E extends TreeNode> {
         .deleteSubTree(node)
         .then((value) => onRemove?.call(context, node));
   }
-
 }
 
 class TreeListView<E extends TreeNode> extends StatelessWidget {
@@ -58,6 +74,7 @@ class TreeListView<E extends TreeNode> extends StatelessWidget {
   final void Function(BuildContext context, E parent, E node)? onAdd;
   final void Function(BuildContext context, E source, E? target, bool result)? onReorder;
   final ListTile? listTile;
+  final ListTile? editListTile;
   final String Function(E node) title;
   final String Function(E node)? subTitle;
 
@@ -71,7 +88,10 @@ class TreeListView<E extends TreeNode> extends StatelessWidget {
       this.onTapped,
       required this.title,
       this.subTitle,
-      this.listTile})
+      this.listTile,
+        this.editListTile
+      }
+        )
       : super(key: key);
 
   void _onAdd(BuildContext context, E parent) {
@@ -128,20 +148,7 @@ class TreeListView<E extends TreeNode> extends StatelessWidget {
         final int rootLevel = root?.level ?? 0;
         final node = model.nodes[index];
         // print(node);
-        return ListTile(
-          key: Key('__NodeItem__${node.id}__'),
-          onTap: () => onTapped?.call(node.id),
-          leading: Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
-            IconButton(
-              icon: Icon(node.expanded ? Icons.arrow_drop_down : Icons.arrow_right),
-              onPressed: () => Provider.of<TreeListModel<E>>(context, listen: false).toggleExpandNode(node),
-            ),
-            Container(padding: EdgeInsets.symmetric(horizontal: (node.level - rootLevel) * 10.0)),
-            // Icon(Icons.arrow_right),
-          ]),
-          title: Text(title(node)),
-          subtitle: subTitle != null ? Text(subTitle?.call(node) ?? '') : null,
-        );
+        return listTile ?? buildListTile(node, context, rootLevel);
       },
     );
   }
@@ -160,25 +167,61 @@ class TreeListView<E extends TreeNode> extends StatelessWidget {
         return Dismissible(
           key: Key('__ModNodeItem__${node.id}__'),
           onDismissed: (_) => _onRemove(context, node),
-          child: ListTile(
-            onTap: () => onTapped?.call(node.id),
-            leading: Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
-              IconButton(
-                icon: Icon(node.expanded ? Icons.arrow_drop_down : Icons.arrow_right),
-                onPressed: () => Provider.of<TreeListModel<E>>(context, listen: false).toggleExpandNode(node),
-              ),
-              IconButton(icon: const Icon(Icons.add), onPressed: () => _onAdd(context, node)),
-              IconButton(icon: const Icon(Icons.delete), onPressed: () => _onRemove(context, node)),
-              Container(padding: EdgeInsets.symmetric(horizontal: (node.level - rootLevel) * 10.0)),
-              Icon(Icons.arrow_right),
-            ]),
-            title: Text(
-              title(node),
-            ),
-            subtitle: subTitle != null ? Text(subTitle?.call(node) ?? '') : null,
-          ),
+          child: editListTile ?? buildEditListTile(context, node, rootLevel),
         );
       },
     );
+  }
+
+  ListTile buildListTile(node, BuildContext context, int rootLevel) {
+    return ListTile(
+        key: Key('__NodeItem__${node.id}__'),
+        onTap: () => onTapped?.call(node.id),
+        // leading: Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+        //   IconButton(
+        //     icon: Icon(node.expanded ? Icons.arrow_drop_down : Icons.arrow_right),
+        //     onPressed: () => Provider.of<TreeListModel<E>>(context, listen: false).toggleExpandNode(node),
+        //   ),
+        //   Container(padding: EdgeInsets.symmetric(horizontal: (node.level - rootLevel) * 10.0)),
+        //   // Icon(Icons.arrow_right),
+        // ]),
+        leading: IconButton(
+          icon: Icon(node.expanded ? Icons.indeterminate_check_box_outlined : Icons.add_box_outlined),
+          onPressed: () => Provider.of<TreeListModel<E>>(context, listen: false).toggleExpandNode(node),
+        ),
+        title: Text(title(node)),
+        subtitle: subTitle != null ? Text(subTitle?.call(node) ?? '') : null,
+        contentPadding: EdgeInsets.only(left: (node.level - rootLevel) * 12.0),
+      );
+  }
+
+  ListTile buildEditListTile(BuildContext context, E node, int rootLevel) {
+    return ListTile(
+          onTap: () => onTapped?.call(node.id),
+          leading:IconButton(
+            icon: Icon(node.expanded ? Icons.indeterminate_check_box_outlined : Icons.add_box_outlined),
+          onPressed: () => Provider.of<TreeListModel<E>>(context, listen: false).toggleExpandNode(node),
+        ),
+          contentPadding: EdgeInsets.only(left: (node.level - rootLevel) * 12.0),
+          trailing: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, mainAxisSize: MainAxisSize.min, children: <Widget>[
+            IconButton(icon: const Icon(Icons.add), onPressed: () => _onAdd(context, node)),
+            IconButton(icon: const Icon(Icons.delete), onPressed: () => _onRemove(context, node)),
+            if (kIsWeb) Padding(padding: EdgeInsets.only(left: 40.0))
+          ]),
+          // leading: Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+          //   IconButton(
+          //     icon: Icon(node.expanded ? Icons.arrow_drop_down : Icons.arrow_right),
+          //     onPressed: () => Provider.of<TreeListModel<E>>(context, listen: false).toggleExpandNode(node),
+          //   ),
+          //   IconButton(icon: const Icon(Icons.add), onPressed: () => _onAdd(context, node)),
+          //   IconButton(icon: const Icon(Icons.delete), onPressed: () => _onRemove(context, node)),
+          //   Container(padding: EdgeInsets.symmetric(horizontal: (node.level - rootLevel) * 10.0)),
+          //   Icon(Icons.arrow_right),
+          // ]),
+          title: Text(
+            title(node),
+          ),
+          subtitle: subTitle != null ? Text(subTitle?.call(node) ?? '') : null,
+        );
   }
 }
